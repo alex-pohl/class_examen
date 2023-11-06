b75e20c0746b323994e90ad14c4e83e164d29201
alex-pohl <144777159+alex-pohl@users.noreply.github.com>
2023-11-06 12:16:55 +0100

    Add files via upload

---
+++ class.ts
@@ -0,0 +1,66 @@
+/* 
+-Crea una clase que guarde el nombre y número de telefono.
+-Guarda esta información en una array para poder leer y borrar entradas.
+-Crea un metodo de busqueda, borrado, y otro de mostrar la información de la entrada de la agenda.
+*/
+
+let datos: {id: string, nombre: string, telefono: string}[] = [];
+class Agenda{
+        id: string
+        nombre : string
+        telefono: string
+    constructor(_id: string, _nombre: string, _telefono: string){
+        console.log(`Creando contacto con id: ${_id}`)
+        this.id = _id
+        this.nombre = _nombre
+        this.telefono = _telefono
+    }
+
+    //1r metodo: Añadir datos al Array.
+    añadirDatos(){
+        datos.push({id: this.id, nombre: this.nombre, telefono: this.telefono})
+    }
+
+    //2ndo metodo: Mostrar los datos de la agenda.
+    mostrarDatos(){
+        let encontrado: boolean = false;
+        for (let i in datos){
+            if (datos[i].id === this.id){
+                console.log(datos[i]);
+                encontrado = true;
+            }
+        }
+        if (encontrado === false){
+            console.log('Contacto no encontado.')
+        }
+    }
+    //3r metodo: Borrar un contacto.
+    borrarDatos(){
+        for (let i in datos){
+            if (datos[i].id === this.id){
+                let index: number = parseInt(i);
+                datos.splice(index,1);
+            }
+        }
+    }
+    buscarPorId(){
+        for (let i in datos){
+            if (datos[i].id === this.id){
+                console.log('\n\nID: ' + datos[i].id + ' \nNombre: '+ datos[i].nombre + '\nTeléfono: '+ datos[i].telefono)
+            }
+        }
+    }
+}
+
+const contacto1 = new Agenda('contacto1','alex', '655765432');
+const contacto2 = new Agenda('contacto2', 'Julia', '987654321');
+const contacto3 = new Agenda('contacto3', 'Ramon', '135784536');
+const contacto4 = new Agenda('contacto4', 'Lucia', '666444111');
+contacto1.añadirDatos();
+contacto2.añadirDatos();
+contacto3.añadirDatos();
+contacto4.añadirDatos();
+
+contacto3.buscarPorId();
+
+
